345668ec09b8deee2c0a5e772e0afa001ed15377
MoroccanGemsbok <71042550+MoroccanGemsbok@users.noreply.github.com>
2020-09-09 17:05:27 -0400

    First edit for Aegir

---
+++ aegirbotbot.py
@@ -0,0 +1,58 @@
+import discord
+import random
+from discord.ext import commands
+
+
+client = commands.Bot(command_prefix = '.') #Aegirbot command prefix
+
+
+#Tells you when Aegirbot is ready to rock and roll
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Game('Aegir Life'))
+    print('Aegirbot is ready.')
+
+
+#Ping command to find latency
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Fuck off! {round(client.latency * 1000)}ms')
+
+
+#Pic of Aegir command
+@client.command()
+async def pic(ctx):
+    pictures = ['https://imgur.com/F2UhyI4',
+                'https://imgur.com/51YfC6m',
+                'https://imgur.com/M79Ic6z',
+                'https://imgur.com/b9ab84N']
+
+    await ctx.send(f'{random.choice(pictures)}'.format(ctx))
+
+
+#When you say aegir the bot does :at:
+@client.event
+async def on_message(message):
+    await client.process_commands(message)
+    truemessage = message.content.lower() #converts message to all lowercase
+
+    if 'aegir' in truemessage:
+        await message.add_reaction(':at:699437200672292935')
+
+
+'''
+@client.event
+async def on_message(message):
+    aegir1 = 'aegir'  # Word that you want to check in a string
+    aegir2 = 'Aegir'
+    aegir3 = 'AEGIR'
+
+    if aegir1 in message.content:
+        await message.add_reaction(':at:699437200672292935')
+    elif aegir2 in message.content:
+        await message.add_reaction(':at:699437200672292935')
+    elif aegir3 in message.content:
+        await message.add_reaction(':at:699437200672292935')
+'''
+
+client.run('NzUyMTkxNTk2MjE2MzIwMTAw.X1UDAw.5isGbIFuEo0bsFeOB0mGv4Uxo_s')
